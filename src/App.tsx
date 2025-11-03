c3e51e79e1a2f7eccf35a97a5e95ba8a9aff9f4b
minabayat-bit <minabayatt9@gmail.com>
2025-11-03 20:51:24 +0330

    v

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import reactLogo from './assets/react.svg'
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      
+    </>
+  )
+}
+
+export default App
